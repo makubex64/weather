b3d6a689c9971dd4a99e908e690e6a90c293764a
Jorge Adarmes <jorgeluismakubex@gmail.com>
2023-07-09 12:06:45 +0000

    update changes

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -8,11 +8,10 @@ if (key === undefined) {
 }
 
 const keyQuery = `appid=${key}`
-const server = 'http://api.openweathermap.org/data/2.5';
+const server = 'https://api.openweathermap.org/data/2.5';
 
 export async function searchLocation(term: string): Promise<WeatherLocation | undefined> {
   const result = await fetch(`${server}/weather?q=${term}&${keyQuery}`);
-  //console.log(result)
 
   if (result.status === 404) return undefined;
   if (result.status !== 200) throw new Error('Failed to read location data');
@@ -22,7 +21,6 @@ export async function searchLocation(term: string): Promise<WeatherLocation | un
 
 export async function readWeather(locationId: number): Promise<Weather> {
   const current = await fetch(`${server}/weather?id=${locationId}&${keyQuery}&units=metric`);
-  //console.log(current)
 
   if (current.status !== 200) throw new Error('Failed to read location data');
 
@@ -30,7 +28,7 @@ export async function readWeather(locationId: number): Promise<Weather> {
 }
 
 export function getIconUrl(code: string): string {
-  return `http://openweathermap.org/img/wn/${code}.png`;
+  return `https://openweathermap.org/img/wn/${code}.png`;
 }
 
 export async function readForecast(locationId: number): Promise<Weather[]> {
